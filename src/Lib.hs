2cd317b015b9832937c35fe98995e1ffd0c43210
Eirik Halvard Sæther <eirik.halvard.95@gmail.com>
2021-09-26 23:34:58 +0200

    wip: one system to rule them all

--- src/Lib.hs
+++ src/Lib.hs
@@ -9,48 +9,40 @@ import System.Directory
 --  Constants  --
 -----------------
 
-t5Lib :: FilePath
-t5Lib = "/Volumes/EirikT5/Pictures/Fuji/"
-
-jpgFolderName :: String
-jpgFolderName = "01_JPG"
-
-rawFolderName :: String
-rawFolderName = "02_RAW"
-
-exportFolderName :: String
-exportFolderName = "03_EXPORT"
-
 ----------------
 --  Fuji New  --
 ----------------
 
 new :: IO ()
 new = do
-  -- set directory to the external library
-  setCurrentDirectory t5Lib
+  undefined
 
-  -- set directory to the right year
-  year <- getYear
-  let yearDir = "./" ++ year
-  createDirectoryIfMissing False yearDir
-  setCurrentDirectory yearDir
+-- -- set directory to the external library
+-- setCurrentDirectory t5Lib
 
-  -- create folder structure
-  name <- capitalize . trim <$> getLine
-  folderName <- getFolderName name
-  createDirectory folderName
-  createDirectory $ folderName ++ "/" ++ jpgFolderName
-  createDirectory $ folderName ++ "/" ++ rawFolderName
-  createDirectory $ folderName ++ "/" ++ exportFolderName
+-- -- set directory to the right year
+-- year <- getYear
+-- let yearDir = "./" ++ year
+-- createDirectoryIfMissing False yearDir
+-- setCurrentDirectory yearDir
+
+-- -- create folder structure
+-- name <- capitalize . trim <$> getLine
+-- folderName <- getFolderName name
+-- createDirectory folderName
+-- createDirectory $ folderName ++ "/" ++ jpgFolderName
+-- createDirectory $ folderName ++ "/" ++ rawFolderName
+-- createDirectory $ folderName ++ "/" ++ exportFolderName
 
 getYear :: IO String
 getYear = formatTime defaultTimeLocale "%Y" <$> getCurrentTime
 
-getFolderName :: String -> IO String
-getFolderName name = do
-  prefix <- formatTime defaultTimeLocale "%Y-%m-%d | " <$> getCurrentTime
-  return $ prefix ++ name
+getFolderName :: Maybe String -> IO String
+getFolderName Nothing = error "not yet implemented: not able to fetch list of folders and let the user choose an appropriate"
+getFolderName (Just name) = do
+  prefix <- formatTime defaultTimeLocale "%Y-%m-%d - " <$> getCurrentTime
+  let cleanName = capitalize . trim $ name
+  return $ prefix ++ cleanName
 
 ---------------
 --  Helpers  --
@@ -65,4 +57,56 @@ capitalize :: String -> String
 capitalize "" = ""
 capitalize (x : xs) = toUpper x : fmap toLower xs
 
+-- Usage:
+-- fuji --help
+-- fuji transfer --from [from] --to [to]
+-- fuji transfer --all
+-- fuji createAndTransfer
+-- create new folder, transfer photos to folder,
 
+data Env = Env
+  { sdLib :: FilePath,
+    t5Lib :: FilePath,
+    jpgFolderName :: String,
+    rawFolderName :: String,
+    exportFolderName :: String,
+    movieFolderName :: String,
+    year :: String,
+    folderName :: String
+  }
+  deriving (Show, Eq)
+
+getEnv :: Maybe String -> IO Env
+getEnv mName = do
+  year <- getYear
+  folderName <- getFolderName mName
+  return $
+    Env
+      { sdLib = "/Volumes/Untitled/DCIM/147_FUJI/",
+        t5Lib = "/Volumes/EirikT5/Pictures/Fuji/",
+        jpgFolderName = "01_JPG",
+        rawFolderName = "02_RAW",
+        exportFolderName = "03_EXPORT",
+        movieFolderName = "04_MOV",
+        year = year,
+        folderName = folderName
+      }
+
+execute :: Command -> IO ()
+execute (Create name) = do
+  env <- getEnv (Just name)
+  print env
+  error "not finished implementing"
+execute (Transfer transfer) = undefined
+execute (CreateAndTransfer name transfer) = undefined
+
+data Command
+  = Create String
+  | Transfer Transfer
+  | CreateAndTransfer String Transfer
+  deriving (Show, Eq)
+
+data Transfer
+  = RangeTransfer Int Int
+  | AllTransfer
+  deriving (Show, Eq)
